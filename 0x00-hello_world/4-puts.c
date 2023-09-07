43c42010ba75ca05dd9997782054115f8c6dfbd9
youssef <youssefeljouari>
2023-09-07 12:54:09 -0700

    puts, Printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,10 @@
 /**
  * main - Entry point
  *
- * Discription: C code that pirint massege using puts founction
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like buliding a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,10 @@
 /**
  * main - Entry point
  *
- * Discription: C code use printf founction
- *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a pice of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
