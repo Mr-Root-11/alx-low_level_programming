bab7b598866183ceae2abe982852475741d1923c
youssef <youssefeljouari>
2023-09-07 11:51:23 -0700

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is piece of art,\n");
+return (0);
 }
