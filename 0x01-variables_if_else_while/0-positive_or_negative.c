7f7d08baef9627ec01548b0bf2f68c9242da4a2b
youssef <youssefeljouari>
2023-09-08 12:55:36 -0700

    C Variables

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stido.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					if (n == o) {
+					printf("%d\n, is a zero");
+					}
+					else if (n > 0) {
+					printf("%d\n, is postitev");
+					}
+					else
+					printf("is negitev)";
+					return (0);
+}
