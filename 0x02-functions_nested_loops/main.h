2c781779954de809cc5689755950957d3a20bd1e
youssef <youssefeljouari>
2023-09-20 11:35:49 -0700

    More pointers, arrays and strings

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,6 @@
  *
  * Desc: Header file containing declartions for all function
  */
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -17,8 +16,11 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_to_98(int n);
+void print_times_table(int n);
 void print_times_table(int n);
 
 #endif
