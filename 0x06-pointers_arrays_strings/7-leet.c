49de51650e3abc7abae22bc5f6365df6362d84c1
youssef <youssefeljouari>
2023-09-20 12:22:58 -0700

    pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,35 +1,25 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string.
+ * leet - encodes a string into 1337.
  * @str: string pointer
  *
- * Return: string capitalized string.
+ * Return: encoded string.
  */
-char *cap_string(char *str)
+char *leet(char *str)
 {
 	int i = 0, j;
-	char s1, s2;
-	char sp[] = "\t\n,;.!\?\"(){} ";
+	char s;
+	char rs[11] = {"aAeEoOtTlL"};
+	char rd[6] = {"43071"};
 
-	while (str[i] != '\0')
+	while (*(str + i) != '\0')
 	{
-		s1 = *(str + i);
-		if (s1 >= 'a' && s1 <= 'z')
-		{
-			if (i == 0)
-				str[i] = s1 - 32;
-			else
-			{
-				s2 = str[i - 1];
-				for (j = 0; sp[j] != '\0'; ++j)
-				{
-					if (s2 == sp[j])
-						str[i] = s1 - 32;
-				}
-			}
+		s = *(str + i);
+		for (j = 0; j < 10; ++j)
+			if (s == rs[j])
+			*(str + i) = rd[j / 2];
 
-		}
 		i++;
 	}
 	return (str);
