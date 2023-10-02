12c62f63d77b58ba6808dc580ae1237175c7affa
youssef <youssefeljouari>
2023-10-02 15:15:23 -0700

    0xA

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main -  prints all arguments recieved through command line.
+* main - multiplies two numbers recieved through command line.
 * @argc: number of command line arguments
 * @argv: An array containing the program command line arguments
 *
-* Return: Always success.
+* Return: 0 if success otherwise 1.
 */
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc > 0)
+	if (argc - 1 != 2)
 	{
-		for (i = 0; i < argc; ++i)
-			printf("%s\n", argv[i]);
+		printf("Error\n");
+		return (1);
 	}
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 return (0);
 }
