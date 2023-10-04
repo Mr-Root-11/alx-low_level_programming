705a42d272d5798a7e662bf6ed71fbaee6d1b70e
youssef <youssefeljouari>
2023-10-04 12:21:28 -0700

    malloc_free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,10 @@
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-int _strlen(char *string);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int _strlen(char *s);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int len(char *str);
