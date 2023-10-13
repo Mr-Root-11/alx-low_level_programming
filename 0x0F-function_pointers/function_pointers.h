1d06a5891a5e10a58457e7f161e95a20c449f68a
youssef <youssefeljouari>
2023-10-13 09:10:49 -0700

    updated 0x0F

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef POINTERFUNCS
+#define POINTERFUNCS
+
+#include <stdio.h>
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif
+
+#endif /* POINTERFUNCS */
