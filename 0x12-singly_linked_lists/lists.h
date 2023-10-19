a9edf4aa6614a254f59ac1f4d957e8ba7b2deeb3
youssef <youssefeljouari>
2023-10-19 10:36:34 -0700

    stingly linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,11 @@
-#ifndef FILE_LISTS
-#define FILE_LISTS
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
+/** structures here **/
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -10,7 +13,9 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *              
  */
+
 typedef struct list_s
 {
 	char *str;
@@ -18,14 +23,12 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
-
+/** function prototypes here**/
+int _putchar(char);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-void first(void) __attribute__ ((constructor));
 
 #endif
