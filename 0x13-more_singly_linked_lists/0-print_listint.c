754f40348f3331ca08967a5a3c2f3a1d6466e641
youssef <youssefeljouari>
2023-10-24 08:18:01 -0700

    more stringly and linked lists project in C

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- *
- * print_listint print all element to stdout
- * @h: pointer to a singly linked list.
- * Return : an int 
- *
-**/
-size_t print_listint(cons listint_t *h)
+ * print_listint - print all elemement to stdout
+ * @h: pointer to a singly liked list.
+ * Return: an int.
+ **/
+
+size_t print_listint(const listint_t *h)
 {
 	const listint_t *tp;
 	unsigned int cntr = 0;
+
 	tp = h;
 	while (tp)
 	{
